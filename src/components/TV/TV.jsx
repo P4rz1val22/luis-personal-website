900309a9547db4f704d2333779b28bb56e5de060
P4rz1val22 <luis.sar.cor@gmail.com>
2025-05-12 11:13:30 -0400

    testing workflow

--- src/components/TV/TV.jsx
+++ src/components/TV/TV.jsx
@@ -1,77 +1,82 @@
-import './TV.css';
-import React, { useState } from 'react';
+import "./TV.css";
+import React, { useState } from "react";
 
 // The TV component for displaying the main page name. [Home Section]
 const Tv = () => {
+  // The available colors for the screen
+  const gradients = [
+    "repeating-linear-gradient(-48deg, #40456B, #40456B 10px, #393d5d 10px, #393d5d 20px)",
+    "repeating-linear-gradient(-48deg, #FF5733, #FF5733 10px, #C70039 10px, #C70039 20px)",
+    "repeating-linear-gradient(-48deg, #28B463, #28B463 10px, #1D8348 10px, #1D8348 20px)",
+    "repeating-linear-gradient(-48deg, #1F618D, #1F618D 10px, #154360 10px, #154360 20px)",
+    "repeating-linear-gradient(-48deg, #8E44AD, #8E44AD 10px, #5B2C6F 10px, #5B2C6F 20px)",
+  ];
 
-    // The available colors for the screen
-    const gradients = [
-        'repeating-linear-gradient(-48deg, #40456B, #40456B 10px, #393d5d 10px, #393d5d 20px)',
-        'repeating-linear-gradient(-48deg, #FF5733, #FF5733 10px, #C70039 10px, #C70039 20px)',
-        'repeating-linear-gradient(-48deg, #28B463, #28B463 10px, #1D8348 10px, #1D8348 20px)',
-        'repeating-linear-gradient(-48deg, #1F618D, #1F618D 10px, #154360 10px, #154360 20px)',
-        'repeating-linear-gradient(-48deg, #8E44AD, #8E44AD 10px, #5B2C6F 10px, #5B2C6F 20px)',
-    ];
+  // integer background index to check which background is currently in use.
+  const [bgIndex, setBgIndex] = useState(0);
 
-    // integer background index to check which background is currently in use.
-    const [bgIndex, setBgIndex] = useState(0);
+  // Boolean for checking if the background is transitioning.
+  // A.K.A. doing the TV static animation.
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
-    // Boolean for checking if the background is transitioning. 
-    // A.K.A. doing the TV static animation.
-    const [isTransitioning, setIsTransitioning] = useState(false);
+  // Boolean for checking if the button is being hovered over.
+  const [isHoverEnabled, setIsHoverEnabled] = useState(true);
 
-    // Boolean for checking if the button is being hovered over.
-    const [isHoverEnabled, setIsHoverEnabled] = useState(true);
+  // Boolean for checking if the sound can play.
+  const [canPlay, setCanPlay] = useState(true);
 
-    // Boolean for checking if the sound can play.
-    const [canPlay, setCanPlay] = useState(true);
+  // Function for changing the background or "gradient"
+  // referring to the background "lined" style being used.
+  const changeGradient = () => {
+    setIsHoverEnabled(false);
+    setTimeout(() => {
+      setIsHoverEnabled(true);
+    }, 100);
 
-    // Function for changing the background or "gradient" 
-    // referring to the background "lined" style being used.
-    const changeGradient = () => {
+    if (canPlay) {
+      const audio = new Audio("./assets/Sounds/StaticSound.mov");
+      const audio2 = new Audio("./assets/Sounds/ClickSound.mov");
+      audio.volume = 0.6;
+      audio2.volume = 0.6;
+      audio.play();
+      audio2.play();
 
-        setIsHoverEnabled(false);
-        setTimeout(() => {
-            setIsHoverEnabled(true);
-        }, 100);
+      // Prevent further playback
+      setCanPlay(false);
+      // Allow playback after 1 second (1000ms)
+      setTimeout(() => {
+        setCanPlay(true);
+      }, 500);
+    }
+    setIsTransitioning(true);
+    setTimeout(() => {
+      const nextIndex = (bgIndex + 1) % gradients.length;
+      setBgIndex(nextIndex);
+      setIsTransitioning(false);
+    }, 500);
+  };
 
-        if (canPlay) {
-            const audio = new Audio('./assets/Sounds/StaticSound.mov');
-            const audio2 = new Audio('./assets/Sounds/ClickSound.mov');
-            audio.volume = 0.6;
-            audio2.volume = 0.6;
-            audio.play();
-            audio2.play();
-
-            // Prevent further playback
-            setCanPlay(false);
-            // Allow playback after 1 second (1000ms)
-            setTimeout(() => {
-                setCanPlay(true);
-            }, 500);
-        }
-        setIsTransitioning(true);
-        setTimeout(() => {
-            const nextIndex = (bgIndex + 1) % gradients.length;
-            setBgIndex(nextIndex);
-            setIsTransitioning(false);
-        }, 500);
-    };
-
-    // Returns the TV component
-    return (
-        <div>
-            <div className='tv'>
-                <div className={`screen ${isTransitioning ? 'static' : ''}`}
-                    style={{ background: gradients[bgIndex] }}>LUIS&nbsp; <br /> SARMIENTO</div>
-                <div className='hoz-container'>
-                    <div className='lines' />
-                    <div onClick={changeGradient} className={`screenButton ${!isHoverEnabled ? 'screenNoHover' : ''}`} />
-                    <div className='lines Reverse' />
-                </div>
-            </div>
+  // Returns the TV component
+  return (
+    <div>
+      <div className="tv">
+        <div
+          className={`screen ${isTransitioning ? "static" : ""}`}
+          style={{ background: gradients[bgIndex] }}
+        >
+          LUIS&nbsp; <br /> SARMIENTO!
         </div>
-    );
-}
+        <div className="hoz-container">
+          <div className="lines" />
+          <div
+            onClick={changeGradient}
+            className={`screenButton ${!isHoverEnabled ? "screenNoHover" : ""}`}
+          />
+          <div className="lines Reverse" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Tv;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,77 @@
-import Tv from "../components/TV/TV"
-import Printer from "../components/Printer/Printer.jsx"
-import Intercom from "../components/Intercom/Intercom.jsx"
-import './Pages.css';
+import Tv from "../components/TV/TV";
+import Printer from "../components/Printer/Printer.jsx";
+import Intercom from "../components/Intercom/Intercom.jsx";
+import "./Pages.css";
 
 // The Home page .
 const Home = () => {
+  const isMobile = window.innerWidth <= 768;
 
-    const isMobile = window.innerWidth <= 768;
-
-    return (
-        <div>
-            <Tv />
-            <div className="hoz-container heading bottom">
-                <div className='sideLines Blue' />
-                <div className='hiddenElement title'> ABOUT ME</div>
-                <div className='sideLines Reverse Blue' />
-            </div>
-            <div className="img-section">
-                <div className="hoz-container">
-                    <div className={`text-box left " ${isMobile ? 'big' : ''}`}>
-                        <h2>Hello there!</h2> My name is <b>Luis Enrique Sarmiento</b>, a Computer Science student at Northeastern University, currently based in Boston, MA.
-                        Originally from Venezuela and completing my studies in Panama, my unique journey has been fueled by a
-                        passion for tech, problem-solving, and innovation.
-                        <br /><br />
-                        Since I was young, I became entranced with the impact technology had in the world, and became fascinated with its ability
-                        to bring forth social change and improve the lives of others.
-                    </div>
-                    <img className={` ${!isMobile ? 'box left' : 'hide'}`} src='./assets/Images/IMG_1543.jpeg' loading="lazy" />
-                </div>
-                <div className="hoz-container">
-                    <img className={` ${!isMobile ? 'box left' : 'hide'}`} src='./assets/Images/IMG_9476.jpeg' loading="lazy" />
-                    <div className={`text-box left " ${isMobile ? 'big' : ''}`}>
-                        I'm dedicated to using my skills to create tangible, positive change. Whether it's through coding, leading or collaborating
-                        with others, I'm determined to push the boundaries of innovation and create sustainable,
-                        ethical solutions to the problems of today and tomorrow.
-                        <br /><br />
-                        When I'm not buried in code, you might find me exploring the vibrant tech scene
-                        in Boston, travelling abroad or brainstorming new ideas for my next project. I'm excited about the
-                        future and eager to see where my journey in tech will take me next.
-                    </div>
-                </div>
-            </div>
-            <div className="hoz-container heading">
-                <div className='sideLines Left Orange' />
-                <div className='title'> DOCUMENTS</div>
-                <div className='sideLines Reverse Orange' />
-            </div>
-            <Printer />
-
-            <div className="hoz-container heading">
-                <div className='sideLines Orange' />
-                <div className='title'> CONTACT ME</div>
-                <div className='sideLines Reverse Orange' />
-            </div>
-            <Intercom />
+  return (
+    <div>
+      <Tv />
+      <div className="hoz-container heading bottom">
+        <div className="sideLines Blue" />
+        <div className="hiddenElement title">ABOUT ME</div>
+        <div className="sideLines Reverse Blue" />
+      </div>
+      <div className="img-section">
+        <div className="hoz-container">
+          <div className={`text-box left " ${isMobile ? "big" : ""}`}>
+            <h2>Hello there!</h2> My name is <b>Luis Enrique Sarmiento</b>, a
+            Computer Science student at Northeastern University, currently based
+            in Boston, MA. Originally from Venezuela and completing my studies
+            in Panama, my unique journey has been fueled by a passion for tech,
+            problem-solving, and innovation.
             <br />
+            <br />
+            Since I was young, I became entranced with the impact technology had
+            in the world, and became fascinated with its ability to bring forth
+            social change and improve the lives of others.
+          </div>
+          <img
+            className={` ${!isMobile ? "box left" : "hide"}`}
+            src="./assets/Images/IMG_1543.jpeg"
+            loading="lazy"
+          />
         </div>
-    );
-}
+        <div className="hoz-container">
+          <img
+            className={` ${!isMobile ? "box left" : "hide"}`}
+            src="./assets/Images/IMG_9476.jpeg"
+            loading="lazy"
+          />
+          <div className={`text-box left " ${isMobile ? "big" : ""}`}>
+            I'm dedicated to using my skills to create tangible, positive
+            change. Whether it's through coding, leading or collaborating with
+            others, I'm determined to push the boundaries of innovation and
+            create sustainable, ethical solutions to the problems of today and
+            tomorrow.
+            <br />
+            <br />
+            When I'm not buried in code, you might find me exploring the vibrant
+            tech scene in Boston, travelling abroad or brainstorming new ideas
+            for my next project. I'm excited about the future and eager to see
+            where my journey in tech will take me next.
+          </div>
+        </div>
+      </div>
+      <div className="hoz-container heading">
+        <div className="sideLines Left Orange" />
+        <div className="title"> DOCUMENTS</div>
+        <div className="sideLines Reverse Orange" />
+      </div>
+      <Printer />
+
+      <div className="hoz-container heading">
+        <div className="sideLines Orange" />
+        <div className="title"> CONTACT ME</div>
+        <div className="sideLines Reverse Orange" />
+      </div>
+      <Intercom />
+      <br />
+    </div>
+  );
+};
 
 export default Home;
